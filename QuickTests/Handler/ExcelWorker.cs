a57ed434828cc1e88302c261f10c2a50471bcaf8
Evelyn Kolczynski <evelyn.kolczynski@bertelsmann.de>
2020-04-01 20:30:25 +0200

    Changed lib for excel reader

--- QuickTests/Handler/ExcelWorker.cs
+++ QuickTests/Handler/ExcelWorker.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Office.Interop.Excel;
-using System.Net;
 
 namespace QuickTests
 {
@@ -23,58 +21,17 @@ namespace QuickTests
         {
             List<WebTestTask> taskList = new List<WebTestTask>();
 
-            Application xlApp = new Application();
-            Workbook xlWorkbook = xlApp.Workbooks.Open(excelPath);
-            foreach(Worksheet sheet in xlWorkbook.Sheets )
+            var workbook = new XLWorkbook(excelPath);
+
+            var sheet = workbook.Worksheet("Testliste");
+            foreach (var table in sheet.Tables)
             {
-                if(sheet.Name.Equals("Testliste"))
+                if(table.Name.Equals("Testliste"))
                 {
-                    foreach(ListObject table in sheet.ListObjects)
-                    {
-                        if(table.Name.Equals("Testliste"))
-                        {
-                            foreach (ListRow row in table.ListRows)
-                            {
-                                WebTestTask task = new WebTestTask();
-
-                                foreach (ListColumn col in table.ListColumns)
-                                {                                  
-                                    var element = table.Range[row.Index, col.Index];
-
-                                    if (element.Text.Equals("TestScope") || element.Text.Equals("Customer") || element.Text.Equals("URL") || element.Text.Equals("ExpectedHttpStatus"))
-                                    {
-                                        
-                                    }
-                                    else
-                                    {
-                                    if (col.Name.Equals("TestScope")) {
-                                            if(element.Text.Equals("AVAILABILITY"))
-                                            { 
-                                                    task.Task = WebTestTask.TestType.AVAILABILTIY;
-                                            }
-                                        }
-                                        else if (col.Name.Equals("Customer"))
-                                        {
-                                            task.Customer = element.Text;
-                                        }
-                                        else if (col.Name.Equals("URL"))
-                                        {
-                                            task.Url = element.Text;
-                                        }
-                                        else if (col.Name.Equals("ExpectedHttpStatus"))
-                                        {
-                                            task.ExpectedHttpStatus = element.Text;
-                                        }
-                                    }
-                                }
-
-                                if(!String.IsNullOrWhiteSpace(task.Url))
-                                {
-                                    taskList.Add(task);
-                                }
-                            }
-                        }
-                    }
+                    //TODO Enum.TryParse
+                    taskList = table.DataRange.Rows()
+                        .Select(testRow => new WebTestTask(WebTestTask.TestType.AVAILABILTIY, testRow.Field("Customer").GetString(), testRow.Field("URL").GetString(), testRow.Field("ExpectedHttpStatus").GetString()))
+                        .ToList();
                 }
             }
 
